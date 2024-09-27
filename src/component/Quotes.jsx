475354c6b27448a5075fcae0f5ef4389d134b531
mrsahiljaiswal <sahiljaiswal757@gmail.com>
2024-09-28 03:58:40 +0530

    commit

--- src/component/Quotes.jsx
+++ src/component/Quotes.jsx
@@ -3,7 +3,7 @@ import { LuRefreshCw } from "react-icons/lu";
 
 async function fetchQuote(){
     try {
-        const response = await fetch("https://api.quotable.io/random")
+        const response = await fetch("https://api.quotable.io/random?maxLength=200")
         const data = await response.json()
         return data;
     } catch (error) {
